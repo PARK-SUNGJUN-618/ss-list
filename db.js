fac4ca46a1ea5721c102748f8f922edd0a999b83
Sungjun Park <espoir618@gmail.com>
2022-08-11 00:30:09 +0900

    please final server test

--- db.js
+++ db.js
@@ -1,13 +1,20 @@
 const Pool = require("pg").Pool;
 
 // myHeroku
-const proConfig = process.env.DATABASE_URL
+const proConfig = {
+  connectionString : process.env.DATABASE_URL,
+  ssl: {    /* <----- Add SSL option */
+    rejectUnauthorized: false,
+  },
+}
 
 // localDB
-const devConfig = `postgresql://postgres:dbuser@localhost:5432/postgres`
+const devConfig = {
+  connectionString : "postgresql://postgres:dbuser@localhost:5432/postgres"
+}
   
-const pool = new Pool({
-  connectionString: process.env.NODE_ENV === "production" ? proConfig : devConfig
-})  
+const pool = new Pool(
+  process.env.NODE_ENV === "production" ? proConfig : devConfig ,
+)  
     
 module.exports = pool;
