6d434f7feb8b0ec89ab03ce943d44e40097480bb
Sungjun Park <espoir618@gmail.com>
2022-08-27 14:02:03 +0900

    220827 Jun Start SSDiary And Change column type of timestamp

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main';
 import NotFound from './pages/NotFound';
 import SsList from './pages/SsList';
+import SsDiary from './pages/SsDiary';
 import Motion from './pages/Motion';
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Main />}></Route>
           <Route path="/ssList/*" element={<SsList />}></Route>
+          <Route path="/ssDiary/*" element={<SsDiary />}></Route>
           <Route path="/motion/*" element={<Motion />}></Route>
           <Route path="*" element={<NotFound />}></Route>
         </Routes>
--- client/src/pages/Motion.jsx
+++ client/src/pages/Motion.jsx
@@ -23,6 +23,7 @@ export default function Motion() {
   
   useEffect(() => {
     onUpdate();
+    // eslint-disable-next-line react-hooks/exhaustive-deps 
   }, [])
   
 
