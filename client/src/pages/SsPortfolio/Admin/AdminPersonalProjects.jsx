d97993e587a87355ad856547b8ee2a0fa0376c74
Sungjun Park <espoir618@gmail.com>
2024-02-12 03:39:54 +0900

    240212 Jun Edit admin page - spell check

--- client/src/pages/SsPortfolio/Admin/AdminPersonalProjects.jsx
+++ client/src/pages/SsPortfolio/Admin/AdminPersonalProjects.jsx
@@ -222,7 +222,7 @@ export default function AdminProjects() {
                 {/*header*/}
                 <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
                   <h3 className="text-3xl font-semibold">
-                    {selectedItemForEdit ? "Edit Project" : "Add Project"}
+                    {selectedItemForEdit ? "Edit Personal Project" : "Add Personal Project"}
                   </h3>
                   <button
                     className="p-1 ml-auto bg-transparent border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
--- client/src/pages/SsPortfolio/Admin/AdminTeamProjects.jsx
+++ client/src/pages/SsPortfolio/Admin/AdminTeamProjects.jsx
@@ -207,7 +207,7 @@ export default function AdminTeamProjects() {
                 {/*header*/}
                 <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
                   <h3 className="text-3xl font-semibold">
-                    {selectedItemForEdit ? "Edit Experience" : "Add Experience"}
+                    {selectedItemForEdit ? "Edit Team Project" : "Add Team Project"}
                   </h3>
                   <button
                     className="p-1 ml-auto bg-transparent border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
