13f9119e7d6d393b30505b18c5529ba7cbf065fc
Sungjun Park <espoir618@gmail.com>
2024-02-15 19:04:44 +0900

    240215 Jun Edit NotFound Page

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -9,7 +9,7 @@ export default function NotFound() {
           <img src={couple1} alt="" className="w-52 h-52"></img>
         </div>
         <div className="flex flex-col items-center gap-3 text-xl font-[Broadway]">
-          404 error : No page.<br></br>
+          404 error : No page..<br></br>
           <Link to="/">
             <button className="font-black p-3 rounded-md bg-[#BDD7EE] opacity-70 w-52
             transition-colors duration-500 hover:bg-[#5d90bd] active:bg-[#5d90bd]">
